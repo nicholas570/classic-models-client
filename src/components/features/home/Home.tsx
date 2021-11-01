424e0ce5f0828728b53949220ea30ac9d0004a04
nicolas <nmercier@d-edge.com>
2021-11-01 11:35:29 +0100

    clean up

--- src/components/features/home/Home.tsx
+++ src/components/features/home/Home.tsx
@@ -1,20 +1,11 @@
 import { useActor } from '@xstate/react';
-import React, { useContext, useEffect, useState } from 'react';
-import { getEmployees } from '../../../services/api/getEmployees';
+import React, { useContext } from 'react';
 
 import { AuthenticationContext } from '../../contexts/authentication/AuthenticationProvider';
 
 export const Home = () => {
   const { authService } = useContext(AuthenticationContext);
   const [authState, sendToAuthService] = useActor(authService);
-  const [user, setUser] = useState<any>({});
-
-  useEffect(() => {
-    // (async () => {
-    //   const result = await getEmployees();
-    //   setUser(result);
-    // })();
-  }, []);
 
   return <div>Welcome home nicolas</div>;
 };
